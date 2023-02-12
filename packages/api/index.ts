9acab10a3f81d0c6bce070bec519dbf4821012ff
Davi Patricio <davipatricio2006@gmail.com>
2023-02-12 17:49:46 +0000

    add cors

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,10 +1,16 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
 import { loadRoutes } from "./src/routes/index";
 
 const server = fastify();
 
 async function main() {
   await loadRoutes(server);
+
+  await server.register(cors, {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  });
   await server.listen({ port: 4000 });
 
   console.log("Server listening on port 4000");
